2404e5cef5448b218c78def51bb3bb1bfb22b4e1
Antonio Nikishaev <me@lelf.lu>
2019-10-19 21:42:22 +0400

    arity.v

---
+++ arity.v
@@ -0,0 +1,13 @@
+
+Ltac arity x :=
+  let ty := type of x in
+  let arity := match eval red in ty with
+  | forall a b c d e f, _ => 6
+  | forall a b c d e, _   => 5
+  | forall a b c d, _     => 4
+  | forall a b c, _       => 3
+  | forall a b, _         => 2
+  | forall a, _           => 1
+  |  _ => 0
+  end in idtac "arity =" arity ".".
+
